dd75918ee7cb485eb7dcf158ec8f0c6e898e275c
Benjamin S <ben.sommer.skovgaard@gmail.com>
2020-11-14 21:09:32 +0100

    set remote url as permanent url

--- src/base-components/Home.js
+++ src/base-components/Home.js
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
-import { LOCAL_URL, REMOTE_URL } from "../utils/settings";
+import { useState } from "react";
+import { REMOTE_URL } from "../utils/settings";
 import apiFacade from "../base-facades/apiFacade";
 
-export let URL = "";
+export let URL = REMOTE_URL;
 
 export default function Home({isLoggedIn, setBalance}) {
-  const [currentURL, setCurrentURL] = useState(
-    URL === LOCAL_URL ? "Local API" : URL === REMOTE_URL ? "Remote API" : "none"
-  );
   const [msg, setMsg] = useState("");
-
-  useEffect(() => {}, [currentURL]);
-
-  const changeURL = (e) => {
-    URL = e.target.value;
-    if (URL === LOCAL_URL) {
-      setCurrentURL("Local API");
-    } else {
-      setCurrentURL("Remote API");
-    }
-  };
 
   const addToBalance = (e) => {
     e.preventDefault();
@@ -45,16 +31,7 @@ export default function Home({isLoggedIn, setBalance}) {
       <h1>Home</h1>
       <p>This is Benjamin's webshop client. <br />
       Feel free to browse the products in the Products section.
-      </p><br />
-      <p style={{ fontWeight: "bold" }}>
-        Select which API to use <br />
-        Currently using: {currentURL}
       </p>
-      <select onChange={changeURL}>
-        <option value="">Choose...</option>
-        <option value={LOCAL_URL}>Local API</option>
-        <option value={REMOTE_URL}>Remote API</option>
-      </select>
       <br /><br />
       <h3>Log in or register to start shopping!</h3>
     </div>
