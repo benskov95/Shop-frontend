6ad8b401494ec74cde1913b01f128167c0ce980b
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-09-14 17:49:03 +0200

    quick change to fixed position for background

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
 
   return (
     <div className="App" style={{height: "100vh"}}>
+      <div style={{position: "fixed", zIndex: "-1", width: "100%", height: "100vh", top: "0", left: "0", right: "0", backgroundImage: "url(\"https://www.fg-a.com/wallpapers/2018-quatrefoil-background-white.jpg\")"}} />
       <Header
         isLoggedIn={isLoggedIn}
         loginMsg={isLoggedIn ? "Log out" : "Log in"}
